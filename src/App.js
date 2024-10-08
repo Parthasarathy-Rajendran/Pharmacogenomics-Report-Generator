01d0934549eb48e8eafbcdf8b3e6237b8e6ddf65
Parthasarathy-Rajendran <parthasarathyrajendran2003@gmail.com>
2024-10-08 11:52:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 // import ReportGenerator from './components/ReportGenerator';
 import PharmacogenomicsTest from './components/PharmacogenomicsTest';
+// import PdfGenerator from './components/PdfGenerator';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <h1>ReportGenerator</h1>
+        <h1>Report Generator</h1>
       </header>
       <main>
         {/* <ReportGenerator /> */}
         <PharmacogenomicsTest />
+        {/* <PdfGenerator /> */}
       </main>
     </div>
   );
